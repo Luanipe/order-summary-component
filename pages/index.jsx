8609de750aa4d2e54d3ebd2e2010e59b56c6f079
Luanipe <luan.araujo926@gmail.com>
2022-12-18 00:08:08 -0300

    Page title update

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,10 +5,10 @@ export default function Home() {
     return (
         <div>
             <Head>
-                <title>Create Next App</title>
+                <title>Order Summary Component</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            
+
             <main>
                 <OrderSummary/>  
             </main>
