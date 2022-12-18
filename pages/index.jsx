cbc5cb36a1a95ed6adc25116b999908655e2f659
Luanipe <luan.araujo926@gmail.com>
2022-12-18 00:07:06 -0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,17 @@
+import Head from 'next/head';
 import OrderSummary from '../components/OrderSummary';
 
 export default function Home() {
     return (
         <div>
-            <OrderSummary/>
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            
+            <main>
+                <OrderSummary/>  
+            </main>
         </div>
     );
 }
